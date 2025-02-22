5ab631d7a8a1ee4a7bcd0da7c6789846849ef144
momo5502 <mauriceheumann@gmail.com>
2025-02-22 17:16:08 +0100

    Fix formatting

--- src/common/utils/io.cpp
+++ src/common/utils/io.cpp
@@ -4,214 +4,214 @@
 
 namespace utils::io
 {
-	bool remove_file(const std::filesystem::path& file)
-	{
-		if (DeleteFileW(file.wstring().data()) != FALSE)
-		{
-			return true;
-		}
+    bool remove_file(const std::filesystem::path& file)
+    {
+        if (DeleteFileW(file.wstring().data()) != FALSE)
+        {
+            return true;
+        }
 
-		return GetLastError() == ERROR_FILE_NOT_FOUND;
-	}
+        return GetLastError() == ERROR_FILE_NOT_FOUND;
+    }
 
-	bool move_file(const std::filesystem::path& src, const std::filesystem::path& target)
-	{
-		return MoveFileW(src.wstring().data(), target.wstring().data()) == TRUE;
-	}
+    bool move_file(const std::filesystem::path& src, const std::filesystem::path& target)
+    {
+        return MoveFileW(src.wstring().data(), target.wstring().data()) == TRUE;
+    }
 
-	bool file_exists(const std::string& file)
-	{
-		return std::ifstream(file).good();
-	}
+    bool file_exists(const std::string& file)
+    {
+        return std::ifstream(file).good();
+    }
 
-	bool write_file(const std::string& file, const std::string& data, const bool append)
-	{
-		const auto pos = file.find_last_of("/\\");
-		if (pos != std::string::npos)
-		{
-			create_directory(file.substr(0, pos));
-		}
+    bool write_file(const std::string& file, const std::string& data, const bool append)
+    {
+        const auto pos = file.find_last_of("/\\");
+        if (pos != std::string::npos)
+        {
+            create_directory(file.substr(0, pos));
+        }
 
-		std::ofstream stream(
-			file, std::ios::binary | std::ofstream::out | (append ? std::ofstream::app : 0));
+        std::ofstream stream(file, std::ios::binary | std::ofstream::out | (append ? std::ofstream::app : 0));
 
-		if (stream.is_open())
-		{
-			stream.write(data.data(), static_cast<std::streamsize>(data.size()));
-			stream.close();
-			return true;
-		}
+        if (stream.is_open())
+        {
+            stream.write(data.data(), static_cast<std::streamsize>(data.size()));
+            stream.close();
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	std::string read_file(const std::string& file)
-	{
-		std::string data;
-		read_file(file, &data);
-		return data;
-	}
+    std::string read_file(const std::string& file)
+    {
+        std::string data;
+        read_file(file, &data);
+        return data;
+    }
 
-	bool read_file(const std::string& file, std::string* data)
-	{
-		if (!data) return false;
-		data->clear();
+    bool read_file(const std::string& file, std::string* data)
+    {
+        if (!data)
+            return false;
+        data->clear();
 
-		if (file_exists(file))
-		{
-			std::ifstream stream(file, std::ios::binary);
-			if (!stream.is_open()) return false;
+        if (file_exists(file))
+        {
+            std::ifstream stream(file, std::ios::binary);
+            if (!stream.is_open())
+                return false;
 
-			stream.seekg(0, std::ios::end);
-			const std::streamsize size = stream.tellg();
-			stream.seekg(0, std::ios::beg);
+            stream.seekg(0, std::ios::end);
+            const std::streamsize size = stream.tellg();
+            stream.seekg(0, std::ios::beg);
 
-			if (size > -1)
-			{
-				data->resize(static_cast<std::uint32_t>(size));
-				stream.read(data->data(), size);
-				stream.close();
-				return true;
-			}
-		}
+            if (size > -1)
+            {
+                data->resize(static_cast<std::uint32_t>(size));
+                stream.read(data->data(), size);
+                stream.close();
+                return true;
+            }
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	std::size_t file_size(const std::string& file)
-	{
-		if (file_exists(file))
-		{
-			std::ifstream stream(file, std::ios::binary);
+    std::size_t file_size(const std::string& file)
+    {
+        if (file_exists(file))
+        {
+            std::ifstream stream(file, std::ios::binary);
 
-			if (stream.good())
-			{
-				stream.seekg(0, std::ios::end);
-				return static_cast<std::size_t>(stream.tellg());
-			}
-		}
+            if (stream.good())
+            {
+                stream.seekg(0, std::ios::end);
+                return static_cast<std::size_t>(stream.tellg());
+            }
+        }
 
-		return 0;
-	}
+        return 0;
+    }
 
-	bool create_directory(const std::filesystem::path& directory)
-	{
-		return std::filesystem::create_directories(directory);
-	}
+    bool create_directory(const std::filesystem::path& directory)
+    {
+        return std::filesystem::create_directories(directory);
+    }
 
-	bool directory_exists(const std::filesystem::path& directory)
-	{
-		return std::filesystem::is_directory(directory);
-	}
+    bool directory_exists(const std::filesystem::path& directory)
+    {
+        return std::filesystem::is_directory(directory);
+    }
 
-	bool directory_is_empty(const std::filesystem::path& directory)
-	{
-		return std::filesystem::is_empty(directory);
-	}
+    bool directory_is_empty(const std::filesystem::path& directory)
+    {
+        return std::filesystem::is_empty(directory);
+    }
 
-	void copy_folder(const std::filesystem::path& src, const std::filesystem::path& target)
-	{
-		std::filesystem::copy(src, target,
-		                      std::filesystem::copy_options::overwrite_existing |
-		                      std::filesystem::copy_options::recursive);
-	}
+    void copy_folder(const std::filesystem::path& src, const std::filesystem::path& target)
+    {
+        std::filesystem::copy(
+            src, target, std::filesystem::copy_options::overwrite_existing | std::filesystem::copy_options::recursive);
+    }
 
+    bool file_exists(const std::wstring& file)
+    {
+        return std::ifstream(file).good();
+    }
 
-	bool file_exists(const std::wstring& file)
-	{
-		return std::ifstream(file).good();
-	}
+    bool write_file(const std::wstring& file, const std::string& data, const bool append)
+    {
+        const auto pos = file.find_last_of(L"/\\");
+        if (pos != std::string::npos)
+        {
+            create_directory(file.substr(0, pos));
+        }
 
-	bool write_file(const std::wstring& file, const std::string& data, const bool append)
-	{
-		const auto pos = file.find_last_of(L"/\\");
-		if (pos != std::string::npos)
-		{
-			create_directory(file.substr(0, pos));
-		}
+        std::ofstream stream(file, std::ios::binary | std::ofstream::out | (append ? std::ofstream::app : 0));
 
-		std::ofstream stream(
-			file, std::ios::binary | std::ofstream::out | (append ? std::ofstream::app : 0));
+        if (stream.is_open())
+        {
+            stream.write(data.data(), static_cast<std::streamsize>(data.size()));
+            stream.close();
+            return true;
+        }
 
-		if (stream.is_open())
-		{
-			stream.write(data.data(), static_cast<std::streamsize>(data.size()));
-			stream.close();
-			return true;
-		}
+        return false;
+    }
 
-		return false;
-	}
+    std::string read_file(const std::wstring& file)
+    {
+        std::string data;
+        read_file(file, &data);
+        return data;
+    }
 
-	std::string read_file(const std::wstring& file)
-	{
-		std::string data;
-		read_file(file, &data);
-		return data;
-	}
+    bool read_file(const std::wstring& file, std::string* data)
+    {
+        if (!data)
+            return false;
+        data->clear();
 
-	bool read_file(const std::wstring& file, std::string* data)
-	{
-		if (!data) return false;
-		data->clear();
+        if (file_exists(file))
+        {
+            std::ifstream stream(file, std::ios::binary);
+            if (!stream.is_open())
+                return false;
 
-		if (file_exists(file))
-		{
-			std::ifstream stream(file, std::ios::binary);
-			if (!stream.is_open()) return false;
+            stream.seekg(0, std::ios::end);
+            const std::streamsize size = stream.tellg();
+            stream.seekg(0, std::ios::beg);
 
-			stream.seekg(0, std::ios::end);
-			const std::streamsize size = stream.tellg();
-			stream.seekg(0, std::ios::beg);
+            if (size > -1)
+            {
+                data->resize(static_cast<uint32_t>(size));
+                stream.read(data->data(), size);
+                stream.close();
+                return true;
+            }
+        }
 
-			if (size > -1)
-			{
-				data->resize(static_cast<uint32_t>(size));
-				stream.read(data->data(), size);
-				stream.close();
-				return true;
-			}
-		}
+        return false;
+    }
 
-		return false;
-	}
+    std::size_t file_size(const std::wstring& file)
+    {
+        if (file_exists(file))
+        {
+            std::ifstream stream(file, std::ios::binary);
 
-	std::size_t file_size(const std::wstring& file)
-	{
-		if (file_exists(file))
-		{
-			std::ifstream stream(file, std::ios::binary);
+            if (stream.good())
+            {
+                stream.seekg(0, std::ios::end);
+                return static_cast<std::size_t>(stream.tellg());
+            }
+        }
 
-			if (stream.good())
-			{
-				stream.seekg(0, std::ios::end);
-				return static_cast<std::size_t>(stream.tellg());
-			}
-		}
+        return 0;
+    }
 
-		return 0;
-	}
+    std::vector<std::filesystem::path> list_files(const std::filesystem::path& directory, const bool recursive)
+    {
+        std::error_code code{};
+        std::vector<std::filesystem::path> files;
 
-	std::vector<std::filesystem::path> list_files(const std::filesystem::path& directory, const bool recursive)
-	{
-		std::error_code code{};
-		std::vector<std::filesystem::path> files;
+        if (recursive)
+        {
+            for (auto& file : std::filesystem::recursive_directory_iterator(directory, code))
+            {
+                files.push_back(file.path());
+            }
+        }
+        else
+        {
+            for (auto& file : std::filesystem::directory_iterator(directory, code))
+            {
+                files.push_back(file.path());
+            }
+        }
 
-		if (recursive)
-		{
-			for (auto& file : std::filesystem::recursive_directory_iterator(directory, code))
-			{
-				files.push_back(file.path());
-			}
-		}
-		else
-		{
-			for (auto& file : std::filesystem::directory_iterator(directory, code))
-			{
-				files.push_back(file.path());
-			}
-		}
-
-		return files;
-	}
+        return files;
+    }
 }
--- src/common/utils/string.cpp
+++ src/common/utils/string.cpp
@@ -8,245 +8,243 @@
 
 namespace utils::string
 {
-	const char* va(const char* fmt, ...)
-	{
-		static thread_local va_provider<8, 256> provider;
+    const char* va(const char* fmt, ...)
+    {
+        static thread_local va_provider<8, 256> provider;
 
-		va_list ap;
-		va_start(ap, fmt);
+        va_list ap;
+        va_start(ap, fmt);
 
-		const char* result = provider.get(fmt, ap);
+        const char* result = provider.get(fmt, ap);
 
-		va_end(ap);
-		return result;
-	}
+        va_end(ap);
+        return result;
+    }
 
-	std::vector<std::string> split(const std::string& s, const char delim)
-	{
-		std::stringstream ss(s);
-		std::string item;
-		std::vector<std::string> elems;
+    std::vector<std::string> split(const std::string& s, const char delim)
+    {
+        std::stringstream ss(s);
+        std::string item;
+        std::vector<std::string> elems;
 
-		while (std::getline(ss, item, delim))
-		{
-			elems.push_back(item); // elems.push_back(std::move(item)); // if C++11 (based on comment from @mchiasson)
-		}
+        while (std::getline(ss, item, delim))
+        {
+            elems.push_back(item); // elems.push_back(std::move(item)); // if C++11 (based on comment from @mchiasson)
+        }
 
-		return elems;
-	}
+        return elems;
+    }
 
-	std::string to_lower(std::string text)
-	{
-		std::transform(text.begin(), text.end(), text.begin(), [](const unsigned char input)
-		{
-			return static_cast<char>(std::tolower(input));
-		});
+    std::string to_lower(std::string text)
+    {
+        std::transform(text.begin(), text.end(), text.begin(),
+                       [](const unsigned char input) { return static_cast<char>(std::tolower(input)); });
 
-		return text;
-	}
+        return text;
+    }
 
-	std::string to_upper(std::string text)
-	{
-		std::transform(text.begin(), text.end(), text.begin(), [](const unsigned char input)
-		{
-			return static_cast<char>(std::toupper(input));
-		});
+    std::string to_upper(std::string text)
+    {
+        std::transform(text.begin(), text.end(), text.begin(),
+                       [](const unsigned char input) { return static_cast<char>(std::toupper(input)); });
 
-		return text;
-	}
+        return text;
+    }
 
-	bool starts_with(const std::string& text, const std::string& substring)
-	{
-		return text.find(substring) == 0;
-	}
+    bool starts_with(const std::string& text, const std::string& substring)
+    {
+        return text.find(substring) == 0;
+    }
 
-	bool ends_with(const std::string& text, const std::string& substring)
-	{
-		if (substring.size() > text.size()) return false;
-		return std::equal(substring.rbegin(), substring.rend(), text.rbegin());
-	}
+    bool ends_with(const std::string& text, const std::string& substring)
+    {
+        if (substring.size() > text.size())
+            return false;
+        return std::equal(substring.rbegin(), substring.rend(), text.rbegin());
+    }
 
-	bool is_numeric(const std::string& text)
-	{
-		auto it = text.begin();
-		while (it != text.end() && std::isdigit(static_cast<unsigned char>(*it)))
-		{
-			++it;
-		}
+    bool is_numeric(const std::string& text)
+    {
+        auto it = text.begin();
+        while (it != text.end() && std::isdigit(static_cast<unsigned char>(*it)))
+        {
+            ++it;
+        }
 
-		return !text.empty() && it == text.end();
-	}
+        return !text.empty() && it == text.end();
+    }
 
-	std::string dump_hex(const std::string& data, const std::string& separator)
-	{
-		std::string result;
+    std::string dump_hex(const std::string& data, const std::string& separator)
+    {
+        std::string result;
 
-		for (unsigned int i = 0; i < data.size(); ++i)
-		{
-			if (i > 0)
-			{
-				result.append(separator);
-			}
+        for (unsigned int i = 0; i < data.size(); ++i)
+        {
+            if (i > 0)
+            {
+                result.append(separator);
+            }
 
-			result.append(va("%02X", data[i] & 0xFF));
-		}
+            result.append(va("%02X", data[i] & 0xFF));
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	std::string get_clipboard_data()
-	{
-		if (OpenClipboard(nullptr))
-		{
-			std::string data;
+    std::string get_clipboard_data()
+    {
+        if (OpenClipboard(nullptr))
+        {
+            std::string data;
 
-			auto* const clipboard_data = GetClipboardData(1u);
-			if (clipboard_data)
-			{
-				auto* const cliptext = static_cast<char*>(GlobalLock(clipboard_data));
-				if (cliptext)
-				{
-					data.append(cliptext);
-					GlobalUnlock(clipboard_data);
-				}
-			}
-			CloseClipboard();
+            auto* const clipboard_data = GetClipboardData(1u);
+            if (clipboard_data)
+            {
+                auto* const cliptext = static_cast<char*>(GlobalLock(clipboard_data));
+                if (cliptext)
+                {
+                    data.append(cliptext);
+                    GlobalUnlock(clipboard_data);
+                }
+            }
+            CloseClipboard();
 
-			return data;
-		}
-		return {};
-	}
+            return data;
+        }
+        return {};
+    }
 
-	void strip(const char* in, char* out, size_t max)
-	{
-		assert(max);
-		if (!in || !out) return;
+    void strip(const char* in, char* out, size_t max)
+    {
+        assert(max);
+        if (!in || !out)
+            return;
 
-		max--;
-		size_t current = 0;
-		while (*in != '\0' && current < max)
-		{
-			const auto color_index = (*(in + 1) - 48) >= 0xC ? 7 : (*(in + 1) - 48);
+        max--;
+        size_t current = 0;
+        while (*in != '\0' && current < max)
+        {
+            const auto color_index = (*(in + 1) - 48) >= 0xC ? 7 : (*(in + 1) - 48);
 
-			if (*in == '^' && (color_index != 7 || *(in + 1) == '7'))
-			{
-				++in;
-			}
-			else
-			{
-				*out = *in;
-				++out;
-				++current;
-			}
+            if (*in == '^' && (color_index != 7 || *(in + 1) == '7'))
+            {
+                ++in;
+            }
+            else
+            {
+                *out = *in;
+                ++out;
+                ++current;
+            }
 
-			++in;
-		}
+            ++in;
+        }
 
-		*out = '\0';
-	}
+        *out = '\0';
+    }
 
-	void strip_material(const char* in, char* out, size_t max)
-	{
-		assert(max);
-		if (!in || !out) return;
+    void strip_material(const char* in, char* out, size_t max)
+    {
+        assert(max);
+        if (!in || !out)
+            return;
 
-		size_t i = 0;
-		while (*in != '\0' && i < max - 1)
-		{
-			if (*in != '$' && *in != '{' && *in != '}')
-			{
-				*out++ = *in;
-				++i;
-			}
-			++in;
-		}
+        size_t i = 0;
+        while (*in != '\0' && i < max - 1)
+        {
+            if (*in != '$' && *in != '{' && *in != '}')
+            {
+                *out++ = *in;
+                ++i;
+            }
+            ++in;
+        }
 
-		*out = '\0';
-	}
+        *out = '\0';
+    }
 
-	std::string convert(const std::wstring& wstr)
-	{
-		const int count = WideCharToMultiByte(CP_UTF8, 0, wstr.data(), static_cast<int>(wstr.size()), nullptr, 0, nullptr, nullptr);
+    std::string convert(const std::wstring& wstr)
+    {
+        const int count =
+            WideCharToMultiByte(CP_UTF8, 0, wstr.data(), static_cast<int>(wstr.size()), nullptr, 0, nullptr, nullptr);
 
-		std::string str(count, 0);
-		WideCharToMultiByte(CP_UTF8, 0, wstr.data(), static_cast<int>(wstr.size()), str.data(), count, nullptr, nullptr);
+        std::string str(count, 0);
+        WideCharToMultiByte(CP_UTF8, 0, wstr.data(), static_cast<int>(wstr.size()), str.data(), count, nullptr,
+                            nullptr);
 
-		return str;
-	}
+        return str;
+    }
 
-	std::wstring convert(const std::string& str)
-	{
-		const auto count = MultiByteToWideChar(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), nullptr, 0);
+    std::wstring convert(const std::string& str)
+    {
+        const auto count = MultiByteToWideChar(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), nullptr, 0);
 
-		std::wstring wstr(count, 0);
-		MultiByteToWideChar(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), wstr.data(), count);
+        std::wstring wstr(count, 0);
+        MultiByteToWideChar(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), wstr.data(), count);
 
-		return wstr;
-	}
+        return wstr;
+    }
 
-	std::string replace(std::string str, const std::string& from, const std::string& to)
-	{
-		if (from.empty())
-		{
-			return str;
-		}
+    std::string replace(std::string str, const std::string& from, const std::string& to)
+    {
+        if (from.empty())
+        {
+            return str;
+        }
 
-		size_t start_pos = 0;
-		while ((start_pos = str.find(from, start_pos)) != std::string::npos)
-		{
-			str.replace(start_pos, from.length(), to);
-			start_pos += to.length();
-		}
+        size_t start_pos = 0;
+        while ((start_pos = str.find(from, start_pos)) != std::string::npos)
+        {
+            str.replace(start_pos, from.length(), to);
+            start_pos += to.length();
+        }
 
-		return str;
-	}
+        return str;
+    }
 
-	std::string& ltrim(std::string& str)
-	{
-		str.erase(str.begin(), std::find_if(str.begin(), str.end(), [](const unsigned char input)
-		{
-			return !std::isspace(input);
-		}));
+    std::string& ltrim(std::string& str)
+    {
+        str.erase(str.begin(),
+                  std::find_if(str.begin(), str.end(), [](const unsigned char input) { return !std::isspace(input); }));
 
-		return str;
-	}
+        return str;
+    }
 
-	std::string& rtrim(std::string& str)
-	{
-		str.erase(std::find_if(str.rbegin(), str.rend(), [](const  unsigned char input)
-		{
-			return !std::isspace(input);
-		}).base(), str.end());
+    std::string& rtrim(std::string& str)
+    {
+        str.erase(std::find_if(str.rbegin(), str.rend(), [](const unsigned char input) { return !std::isspace(input); })
+                      .base(),
+                  str.end());
 
-		return str;
-	}
+        return str;
+    }
 
-	void trim(std::string& str)
-	{
-		ltrim(rtrim(str));
-	}
+    void trim(std::string& str)
+    {
+        ltrim(rtrim(str));
+    }
 
-	void copy(char* dest, const size_t max_size, const char* src)
-	{
-		if (!max_size)
-		{
-			return;
-		}
+    void copy(char* dest, const size_t max_size, const char* src)
+    {
+        if (!max_size)
+        {
+            return;
+        }
 
-		for (size_t i = 0;; ++i)
-		{
-			if (i + 1 == max_size)
-			{
-				dest[i] = 0;
-				break;
-			}
+        for (size_t i = 0;; ++i)
+        {
+            if (i + 1 == max_size)
+            {
+                dest[i] = 0;
+                break;
+            }
 
-			dest[i] = src[i];
+            dest[i] = src[i];
 
-			if (!src[i])
-			{
-				break;
-			}
-		}
-	}
+            if (!src[i])
+            {
+                break;
+            }
+        }
+    }
 }
