545e94a94179569a7ca765b1fe27882ec8517f1c
momo5502 <mauriceheumann@gmail.com>
2025-03-01 17:52:27 +0100

    Hook exit

--- src/overlay/main.cpp
+++ src/overlay/main.cpp
@@ -1,19 +1,45 @@
 #include "overlay.hpp"
 
 #include <utils/win.hpp>
+#include <utils/hook.hpp>
 
 #include <cef_include.hpp>
 
 namespace
 {
+    utils::hook::detour exit_hook{};
+
     std::unique_ptr<utils::object>& get_overlay()
     {
         static std::unique_ptr<utils::object> overlay{};
         return overlay;
     }
 
+    void on_exit()
+    {
+        get_overlay() = {};
+    }
+
+    [[noreturn]] void NTAPI rtl_exit_user_process_stub(const DWORD exit_status)
+    {
+        auto* orig = exit_hook.get_place();
+        exit_hook.clear();
+
+        on_exit();
+
+        static_cast<decltype(&rtl_exit_user_process_stub)>(orig)(exit_status);
+    }
+
+    void install_exit_hook()
+    {
+        const utils::nt::library ntdll("ntdll.dll");
+        auto* exit_process = ntdll.get_proc<void*>("RtlExitUserProcess");
+        exit_hook.create(exit_process, rtl_exit_user_process_stub);
+    }
+
     DWORD WINAPI initialize_async(LPVOID)
     {
+        install_exit_hook();
         get_overlay() = gameoverlay::create_overlay();
         return 0;
     }
