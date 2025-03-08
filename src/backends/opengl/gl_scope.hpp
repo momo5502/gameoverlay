2b2f98881d6fd3401ca6796295c67871008c6d0a
momo5502 <mauriceheumann@gmail.com>
2025-03-08 10:36:04 +0100

    Store vao

--- src/backends/opengl/gl_scope.hpp
+++ src/backends/opengl/gl_scope.hpp
@@ -5,12 +5,14 @@
 struct gl_scope : gl_texture_scope
 {
     GLuint program{};
+    GLuint vao{};
 
     gl_scope()
     {
         glPushAttrib(GL_ALL_ATTRIB_BITS);
         glPushClientAttrib(GL_CLIENT_ALL_ATTRIB_BITS);
         glGetIntegerv(GL_CURRENT_PROGRAM, detail::gl_ref(this->program));
+        glGetIntegerv(GL_VERTEX_ARRAY_BINDING, detail::gl_ref(this->vao));
 
         glMatrixMode(GL_TEXTURE);
         glPushMatrix();
@@ -29,6 +31,7 @@ struct gl_scope : gl_texture_scope
         glMatrixMode(GL_TEXTURE);
         glPopMatrix();
 
+        glBindVertexArray(this->vao);
         glUseProgram(this->program);
         glPopClientAttrib();
         glPopAttrib();
