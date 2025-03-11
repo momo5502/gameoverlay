1b864b7f5b1d512c8de362b71345d38a5e012180
momo5502 <mauriceheumann@gmail.com>
2025-03-11 20:22:11 +0100

    FPS counter

--- src/data/main.js
+++ src/data/main.js
@@ -1,5 +1,13 @@
+function blub() {
+    globalThis.count = (globalThis.count || 0) + 1;
+
+    document.querySelector(".notification.info").textContent = "FPS: " + globalThis.count;
+    requestAnimationFrame(blub);
+}
+
 function run() {
     console.log("HI");
+    blub();
 
     setTimeout(() => location.reload(), 1000);
 }
