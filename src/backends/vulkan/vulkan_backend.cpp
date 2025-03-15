c1272b61f58f30e9c446420e310fbce415b76de1
momo5502 <mauriceheumann@gmail.com>
2025-03-15 09:55:06 +0100

    Add more hooks

--- src/backends/vulkan/vulkan_backend.cpp
+++ src/backends/vulkan/vulkan_backend.cpp
@@ -7,40 +7,6 @@ namespace gameoverlay::vulkan
 {
     namespace
     {
-        struct hooks
-        {
-            utils::hook::detour queue_present;
-        };
-
-        hooks& get_hooks()
-        {
-            static hooks h{};
-            return h;
-        }
-
-        // TODO: Synchronize access with object destruction
-        vulkan_backend* g_backend{nullptr};
-
-        void draw_frame(const HDC hdc)
-        {
-            if (!hdc || !g_backend)
-            {
-                return;
-            }
-
-            g_backend->create_or_access_renderer(
-                hdc,
-                [](vulkan_renderer& r) {
-                    r.draw_frame(); //
-                },
-                hdc);
-        }
-
-        VkResult VKAPI_CALL queue_present_stub(const VkQueue queue, const VkPresentInfoKHR* pPresentInfo)
-        {
-            return get_hooks().queue_present.invoke_stdcall<VkResult>(queue, pPresentInfo);
-        }
-
         utils::owned_object<VkInstance> create_vk_instance()
         {
             VkInstanceCreateInfo create_info = {};
@@ -132,6 +98,63 @@ namespace gameoverlay::vulkan
 
             return {device, [](const VkDevice dev) { vkDestroyDevice(dev, nullptr); }};
         }
+
+        struct hooks
+        {
+            utils::hook::detour queue_present{};
+            utils::hook::detour destroy_device{};
+            utils::hook::detour acquire_next_image{};
+            utils::hook::detour acquire_next_image_2{};
+        };
+
+        hooks& get_hooks()
+        {
+            static hooks h{};
+            return h;
+        }
+
+        // TODO: Synchronize access with object destruction
+        vulkan_backend* g_backend{nullptr};
+
+        void draw_frame(const HDC hdc)
+        {
+            if (!hdc || !g_backend)
+            {
+                return;
+            }
+
+            g_backend->create_or_access_renderer(
+                hdc,
+                [](vulkan_renderer& r) {
+                    r.draw_frame(); //
+                },
+                hdc);
+        }
+
+        void VKAPI_CALL destroy_device_stub(const VkDevice device, const VkAllocationCallbacks* allocator)
+        {
+            get_hooks().destroy_device.invoke_stdcall<void>(device, allocator);
+        }
+
+        VkResult VKAPI_CALL acquire_next_image_stub(const VkDevice device, const VkSwapchainKHR swapchain,
+                                                    const uint64_t timeout, const VkSemaphore semaphore,
+                                                    const VkFence fence, uint32_t* image_index)
+        {
+            return get_hooks().acquire_next_image.invoke_stdcall<VkResult>(device, swapchain, timeout, semaphore, fence,
+                                                                           image_index);
+        }
+
+        VkResult VKAPI_CALL acquire_next_image_2_stub(const VkDevice device,
+                                                      const VkAcquireNextImageInfoKHR* acquire_info,
+                                                      uint32_t* image_index)
+        {
+            return get_hooks().acquire_next_image_2.invoke_stdcall<VkResult>(device, acquire_info, image_index);
+        }
+
+        VkResult VKAPI_CALL queue_present_stub(const VkQueue queue, const VkPresentInfoKHR* present_info)
+        {
+            return get_hooks().queue_present.invoke_stdcall<VkResult>(queue, present_info);
+        }
     }
 
     vulkan_backend::~vulkan_backend()
@@ -151,8 +174,17 @@ namespace gameoverlay::vulkan
 
         auto& hooks = get_hooks();
 
+        auto* destroy_device = vkGetDeviceProcAddr(device, "vkDestroyDevice");
+        hooks.destroy_device.create(destroy_device, &destroy_device_stub);
+
         auto* queue_present = vkGetDeviceProcAddr(device, "vkQueuePresentKHR");
         hooks.queue_present.create(queue_present, &queue_present_stub);
+
+        auto* acquire_next_image = vkGetDeviceProcAddr(device, "vkAcquireNextImageKHR");
+        hooks.acquire_next_image.create(acquire_next_image, &acquire_next_image_stub);
+
+        auto* acquire_next_image_2 = vkGetDeviceProcAddr(device, "vkAcquireNextImage2KHR");
+        hooks.acquire_next_image_2.create(acquire_next_image_2, &acquire_next_image_2_stub);
     }
 
     std::unique_ptr<backend> create_backend(backend::owned_handler h)
