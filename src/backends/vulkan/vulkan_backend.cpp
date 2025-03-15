452457af49d510c072d3d620509096f48609678c
momo5502 <mauriceheumann@gmail.com>
2025-03-15 09:38:07 +0100

    Hook vulkan present

--- src/backends/vulkan/vulkan_backend.cpp
+++ src/backends/vulkan/vulkan_backend.cpp
@@ -9,6 +9,7 @@ namespace gameoverlay::vulkan
     {
         struct hooks
         {
+            utils::hook::detour queue_present;
         };
 
         hooks& get_hooks()
@@ -34,6 +35,103 @@ namespace gameoverlay::vulkan
                 },
                 hdc);
         }
+
+        VkResult VKAPI_CALL queue_present_stub(const VkQueue queue, const VkPresentInfoKHR* pPresentInfo)
+        {
+            return get_hooks().queue_present.invoke_stdcall<VkResult>(queue, pPresentInfo);
+        }
+
+        utils::owned_object<VkInstance> create_vk_instance()
+        {
+            VkInstanceCreateInfo create_info = {};
+            constexpr const char* instance_extension = "VK_KHR_surface";
+
+            create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+            create_info.enabledExtensionCount = 1;
+            create_info.ppEnabledExtensionNames = &instance_extension;
+
+            VkInstance instance = VK_NULL_HANDLE;
+            const auto res = vkCreateInstance(&create_info, nullptr, &instance);
+            if (res != VK_SUCCESS)
+            {
+                return {};
+            }
+
+            return {instance, [](const VkInstance i) { vkDestroyInstance(i, nullptr); }};
+        }
+
+        VkPhysicalDevice get_gpu(const VkInstance instance)
+        {
+            uint32_t gpu_count{};
+            vkEnumeratePhysicalDevices(instance, &gpu_count, nullptr);
+
+            std::vector<VkPhysicalDevice> gpus(gpu_count);
+            vkEnumeratePhysicalDevices(instance, &gpu_count, gpus.data());
+
+            if (gpus.empty())
+            {
+                throw std::runtime_error("No gpus found");
+            }
+
+            for (auto& gpu : gpus)
+            {
+                VkPhysicalDeviceProperties properties;
+                vkGetPhysicalDeviceProperties(gpu, &properties);
+                if (properties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+                {
+                    return gpu;
+                }
+            }
+
+            return gpus.front();
+        }
+
+        uint32_t get_queue_family_index(const VkPhysicalDevice gpu)
+        {
+            uint32_t count{};
+            vkGetPhysicalDeviceQueueFamilyProperties(gpu, &count, nullptr);
+
+            std::vector<VkQueueFamilyProperties> queue_families(count);
+            vkGetPhysicalDeviceQueueFamilyProperties(gpu, &count, queue_families.data());
+
+            for (uint32_t i = 0; i < count; ++i)
+            {
+                if (queue_families[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+                {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+
+        utils::owned_object<VkDevice> create_device(const VkPhysicalDevice gpu, const uint32_t queue_family_index)
+        {
+            constexpr float queue_priority = 1.0f;
+            constexpr const char* device_extension = "VK_KHR_swapchain";
+
+            VkDeviceQueueCreateInfo queue_info = {};
+            queue_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+            queue_info.queueFamilyIndex = queue_family_index;
+            queue_info.queueCount = 1;
+            queue_info.pQueuePriorities = &queue_priority;
+
+            VkDeviceCreateInfo create_info = {};
+            create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+            create_info.queueCreateInfoCount = 1;
+            create_info.pQueueCreateInfos = &queue_info;
+            create_info.enabledExtensionCount = 1;
+            create_info.ppEnabledExtensionNames = &device_extension;
+
+            VkDevice device = VK_NULL_HANDLE;
+            const auto res = vkCreateDevice(gpu, &create_info, nullptr, &device);
+            if (res != VK_SUCCESS)
+            {
+                return {};
+            }
+
+            return {device, [](const VkDevice dev) { vkDestroyDevice(dev, nullptr); }};
+        }
     }
 
     vulkan_backend::~vulkan_backend()
@@ -46,15 +144,15 @@ namespace gameoverlay::vulkan
     {
         g_backend = this;
 
-        VkInstanceCreateInfo create_info = {};
-        constexpr const char* instance_extension = "VK_KHR_surface";
+        const auto instance = create_vk_instance();
+        const auto gpu = get_gpu(instance);
+        const auto queue_family_index = get_queue_family_index(gpu);
+        const auto device = create_device(gpu, queue_family_index);
 
-        create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-        create_info.enabledExtensionCount = 1;
-        create_info.ppEnabledExtensionNames = &instance_extension;
+        auto& hooks = get_hooks();
 
-        VkInstance g_Instance{};
-        vkCreateInstance(&create_info, nullptr, &g_Instance);
+        auto* queue_present = vkGetDeviceProcAddr(device, "vkQueuePresentKHR");
+        hooks.queue_present.create(queue_present, &queue_present_stub);
     }
 
     std::unique_ptr<backend> create_backend(backend::owned_handler h)
